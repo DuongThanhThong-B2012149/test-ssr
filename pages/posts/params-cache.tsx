947f310adf269e36b1fb19617cade2dbe0eaa217
thanhthong <thongduong61134@gmail.com>
2022-08-17 22:13:05 +0700

    update ques

--- pages/posts/params-cache.tsx
+++ pages/posts/params-cache.tsx
@@ -13,7 +13,7 @@ export default function ParamsPage({ query, post }: ParamsPageProps) {
 
 	useEffect(() => {
 		const intervalId = setInterval(() => {
-			setSeconds((x) => {
+			setSeconds((x: any) => {
 				if (x > 60) clearInterval(intervalId)
 
 				return x + 1
@@ -29,29 +29,29 @@ export default function ParamsPage({ query, post }: ParamsPageProps) {
 
 			<p>Time: {seconds}s</p>
 
-			<h2>Post detail</h2>
+			{/* <h2>Post detail</h2>
 			<p>{post?.title}</p>
 			<p>{post?.author}</p>
-			<p>{post?.description}</p>
+			<p>{post?.description}</p> */}
 		</div>
 	)
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-	context.res.setHeader('Cache-Control', 's-maxage=10')
+	context.res.setHeader('Cache-Control', 's-maxage=5')
 
 	await new Promise((res) => setTimeout(res, 3000))
 
-	const postId = context.query.postId
-	if (!postId) return { props: { query: context.query } }
+	// const postId = context.query.postId
+	// if (!postId) return { props: { query: context.query } }
 
-	const response = await fetch(`https://js-post-api.herokuapp.com/api/posts/${postId}`)
-	const data = await response.json()
+	// const response = await fetch(`https://js-post-api.herokuapp.com/api/posts/${postId}`)
+	// const data = await response.json()
 
 	return {
 		props: {
 			query: context.query,
-			post: data,
+			post: {},
 		},
 	}
 }
